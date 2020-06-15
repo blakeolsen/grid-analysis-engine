e7de6b136ccafc6e4ff4c926de2feae3d58de819
Blake Olsen <blake.olsen@gmail.com>
2020-06-15 10:52:25 -0400

    First Commit

---
+++ miso_org/client.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import re
+from datetime import date, datetime, timedelta
+import pytz
+
+class DayAheadMarketClient:
+
+    base_url = "https://docs.misoenergy.org/marketreports"
+
+    def __init__(self):
+        pass
+
+    def new_time_series(self, d: date):
+        pd.date_range(d, periods=24, freq='H', tz='EST')
+
+    def day_ahead_lmp(self, d: date, value: str = 'LMP'):
+        url = f"{self.base_url}/{d.strftime('%Y%m%d')}_da_expost_lmp.csv"
+        r = pd.read_csv(url, skiprows=4)
+        indexed = r.set_index('Node')
+        byHour = indexed[indexed['Value'] == value].drop(columns=['Type','Value']).T
+        hours = [int(re.search('\d+', t).group()) for t in byHour.index.values]
+        byHour['ts'] = [datetime(year=d.year, month=d.month, day=d.day) + timedelta(hours=h) for h in hours]
+        timeseries = byHour.set_index('ts')
+        return timeseries
